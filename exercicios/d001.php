43552e116ab0c7c5799ae42aab3875242596df81
Vinícius Martins <102906703+o-vinicius-martins@users.noreply.github.com>
2022-04-07 20:44:52 -0300

    Create d001.php

---
+++ exercicios/d001.php
@@ -0,0 +1,8 @@
+<?php
+//Criar um programa que exiba, na tela do usuário, todos os números ímpares de 0 até 100.
+for ($num = 1; true; $num++) {
+    if ($num % 2 !=0 && $num <= 100) {
+        echo "$num" . "\n";
+    }
+    continue;
+}
