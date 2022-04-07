3627c0d84a84fa6ac2ae6915c036d30b7cf68216
Vinícius Martins <102906703+o-vinicius-martins@users.noreply.github.com>
2022-04-07 20:47:51 -0300

    Update d001.php

--- exercicios/d001.php
+++ exercicios/d001.php
@@ -2,7 +2,7 @@
 //Criar um programa que exiba, na tela do usuário, todos os números ímpares de 0 até 100.
 for ($num = 1; true; $num++) {
     if ($num % 2 !=0 && $num <= 100) {
-        echo "$num" . "\n";
+        echo $num . "\n";
     }
     continue;
 }
